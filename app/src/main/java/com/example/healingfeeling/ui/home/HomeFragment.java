24fbaffed049ea727713ed4b7e12c2cc2f2f9573
serim53 <tpfla9953@naver.com>
2021-02-28 14:27:51 +0900

    recyclerview & tablayout -1

--- app/src/main/java/com/example/healingfeeling/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/healingfeeling/ui/home/HomeFragment.java
@@ -1,5 +1,7 @@
 package com.example.healingfeeling.ui.home;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +13,82 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.healingfeeling.R;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+
+    private TabLayout tabLayout;
+    private ArrayList<String> tabNames = new ArrayList<>();
+
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+                loadTabName();
+                setTabLayout();
+                setViewPager();
+
             }
+
+            @TargetApi(Build.VERSION_CODES.N)
+            private void setTabLayout(){
+                tabLayout = root.findViewById(R.id.tab);
+                tabNames.stream().forEach(name ->tabLayout.addTab(tabLayout.newTab().setText(name)));
+            }
+
+            private void loadTabName(){
+                tabNames.add("Song");
+                tabNames.add("Book");
+                tabNames.add("Place");
+            }
+
+            private void setViewPager() {
+                FragmentAdapter adapter = new FragmentAdapter(getFragmentManager() );
+                ViewPager viewPager = root.findViewById(R.id.viewPager);
+                viewPager.setAdapter(adapter);
+                viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+                tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+                    @Override
+                    public void onTabSelected(TabLayout.Tab tab) {
+                        viewPager.setCurrentItem(tab.getPosition());
+                    }
+                    @Override
+                    public void onTabUnselected(TabLayout.Tab tab) {
+
+                    }
+
+                    @Override
+                    public void onTabReselected(TabLayout.Tab tab) {
+
+                    }
+                });
+            }
+
         });
         return root;
     }
+
+
+
+
 }
---
+++ app/src/main/java/com/example/healingfeeling/ui/home/SongFragment.java
@@ -0,0 +1,108 @@
+package com.example.healingfeeling.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.healingfeeling.R;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SongFragment extends Fragment {
+
+
+    private RecyclerAdapter adapter;
+
+    public static SongFragment newInstance() {
+        SongFragment tab1 = new SongFragment();
+        return tab1;
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View root = inflater.inflate(R.layout.fragment_song, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.recyclerView);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+        getData();
+
+
+        return root;
+    }
+
+
+
+    private void getData() {
+        // 임의의 데이터입니다.
+        List<String> listTitle = Arrays.asList("국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립",
+                "국화", "사막", "수국", "해파리", "코알라", "등대", "펭귄", "튤립");
+        List<String> listContent = Arrays.asList(
+                "이 꽃은 국화입니다.",
+                "여기는 사막입니다.",
+                "이 꽃은 수국입니다.",
+                "이 동물은 해파리입니다.",
+                "이 동물은 코알라입니다.",
+                "이것은 등대입니다.",
+                "이 동물은 펭귄입니다.",
+                "이 꽃은 튤립입니다.",
+                "이 꽃은 국화입니다.",
+                "여기는 사막입니다.",
+                "이 꽃은 수국입니다.",
+                "이 동물은 해파리입니다.",
+                "이 동물은 코알라입니다.",
+                "이것은 등대입니다.",
+                "이 동물은 펭귄입니다.",
+                "이 꽃은 튤립입니다."
+        );
+        List<Integer> listResId = Arrays.asList(
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp,
+                R.drawable.ic_home_black_24dp
+        );
+        for (int i = 0; i < listTitle.size(); i++) {
+            // 각 List의 값들을 data 객체에 set 해줍니다.
+            Data data = new Data();
+            data.setTitle(listTitle.get(i));
+            data.setContent(listContent.get(i));
+            data.setResId(listResId.get(i));
+
+            // 각 값이 들어간 data를 adapter에 추가합니다.
+            adapter.addItem(data);
+        }
+
+        // adapter의 값이 변경되었다는 것을 알려줍니다.
+        adapter.notifyDataSetChanged();
+    }
+
+
+}
