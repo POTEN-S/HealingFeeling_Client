d4c0046221b1e99a0f1b68fe7bd317d8a0cae223
jungaeeum <ghj370000@naver.com>
2021-03-07 23:12:47 +0900

    spinner 구현

--- app/src/main/java/com/example/healingfeeling/PostActivity.java
+++ app/src/main/java/com/example/healingfeeling/PostActivity.java
@@ -25,6 +25,12 @@ public class PostActivity extends AppCompatActivity {
         setContentView(R.layout.fragment_post);
 
         Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.my_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
 
 
         etTitle = (EditText)findViewById(R.id.titleinput);
@@ -50,6 +56,17 @@ public class PostActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                  parent.getItemAtPosition(position);
+                 if (position==0){
+
+                 }
+
+                if (position==1){
+
+                }
+                if (position==2){
+
+                }
+
 
 
             } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
@@ -57,6 +74,16 @@ public class PostActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+
+        });
+
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
