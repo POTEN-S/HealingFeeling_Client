7e5e87abcb1db22d702115bbe63fed2fa2678bff
jungaeeum <ghj370000@naver.com>
2021-04-27 23:39:49 +0900

    마이페이지 회원탈퇴

--- app/src/main/java/com/example/healingfeeling/MypageActivity.java
+++ app/src/main/java/com/example/healingfeeling/MypageActivity.java
@@ -1,13 +1,17 @@
 package com.example.healingfeeling;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.healingfeeling.databinding.ActivityMypageBinding;
 import com.example.healingfeeling.model.User;
@@ -19,13 +23,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Map;
+
 public class MypageActivity extends AppCompatActivity {
+    private static final String TAG = "MypageActivity";
 
     ActivityMypageBinding binding;
-    DatabaseReference mDatabase;
+    DatabaseReference myRef;
     TextView myname;
     FirebaseUser user;
     FirebaseAuth mAuth;
+    FirebaseDatabase database;
 
 
 
@@ -43,14 +51,16 @@ public class MypageActivity extends AppCompatActivity {
 
 
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+        findViewById(R.id.userDeleteButton).setOnClickListener(onClickListener);
 
         myname = (TextView) findViewById(R.id.myPageNickName);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         String uid = user!= null? user.getUid() : null;
         mAuth = FirebaseAuth.getInstance();
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference username = mDatabase.child(uid).child("userName");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("users");
+        DatabaseReference username = myRef.child(uid).child("userName");
 
         username.addValueEventListener(new ValueEventListener() {
             @Override
@@ -61,9 +71,33 @@ public class MypageActivity extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
 
             }
         });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                //데이터를 화면에 출력해 준다.
+                Log.d(TAG, "Value is: " + map);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+
+
+
+
 
     }
 
@@ -75,6 +109,33 @@ public class MypageActivity extends AppCompatActivity {
                     FirebaseAuth.getInstance().signOut();
                     myStartActivity(LoginActivity.class);
                     break;
+                case  R.id.userDeleteButton:
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MypageActivity.this);
+                    builder.setTitle("회원탈퇴").setMessage("정말 회원탈퇴를 하시겠습니까?");
+                    builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //yes 클릭
+                            mAuth.getCurrentUser().delete();
+                            myRef.child("/users").setValue(null);
+                            myStartActivity(LoginActivity.class);
+
+                        }
+                    });
+
+                    builder.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+                        @Override public void onClick(DialogInterface dialog, int which) {
+                            Toast.makeText(getBaseContext(),"취소되었습니다.",Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
+
+                    break;
+
+
             }
         }
     };
@@ -85,4 +146,5 @@ public class MypageActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
 }
