61382a19267dacbd28b08bce9cb50a2e22efedb6
jungaeeum <ghj370000@naver.com>
2021-05-03 18:47:28 +0900

    마이페이지 프로필사진

--- app/src/main/java/com/example/healingfeeling/MypageActivity.java
+++ app/src/main/java/com/example/healingfeeling/MypageActivity.java
@@ -4,9 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,8 +17,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.healingfeeling.databinding.ActivityMypageBinding;
 import com.example.healingfeeling.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -34,7 +41,7 @@ public class MypageActivity extends AppCompatActivity {
     ActivityMypageBinding binding;
     DatabaseReference myRef;
     TextView myname;
-    ImageView iv;
+    ImageView imageView;
     FirebaseUser user;
     FirebaseAuth mAuth;
     FirebaseDatabase database;
@@ -48,6 +55,7 @@ public class MypageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
 
+
         binding= DataBindingUtil.setContentView(this,R.layout.activity_mypage);
 
         binding.progressHappy.setProgress(50);
@@ -59,23 +67,45 @@ public class MypageActivity extends AppCompatActivity {
         findViewById(R.id.userDeleteButton).setOnClickListener(onClickListener);
 
         myname = (TextView) findViewById(R.id.myPageNickName);
-        iv=findViewById(R.id.mypageActivity_imageview_profile);
+        imageView=findViewById(R.id.mypageActivity_imageview_profile);
+
+        User userModel = new User();
+
 
         user = FirebaseAuth.getInstance().getCurrentUser();
+
         String uid = user!= null? user.getUid() : null;
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("users");
         DatabaseReference username = myRef.child(uid).child("userName");
+        DatabaseReference profile_image = myRef.child(uid).child("profileImageUrl");
 
 
+        profile_image.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String profile_url = snapshot.child("image").getValue(String.class);
+                Glide.with(MypageActivity.this).load(profile_url).apply(new RequestOptions().circleCrop())
+                        .into(imageView);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
         username.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+
                 String name = snapshot.getValue(String.class);
                 myname.setText(name);
+
+
             }
 
             @Override
@@ -85,6 +115,10 @@ public class MypageActivity extends AppCompatActivity {
             }
         });
 
+
+
+
+
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -93,6 +127,11 @@ public class MypageActivity extends AppCompatActivity {
                 Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
                 //데이터를 화면에 출력해 준다.
                 Log.d(TAG, "Value is: " + map);
+
+
+
+
+
             }
 
             @Override
@@ -105,6 +144,8 @@ public class MypageActivity extends AppCompatActivity {
 
 
     }
+
+
 
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
@@ -151,7 +192,6 @@ public class MypageActivity extends AppCompatActivity {
 
 
 
-
     public void deleteUserData(final String userName){
         //get userID
         String uid = mAuth.getInstance().getCurrentUser().getUid();
@@ -174,11 +214,18 @@ public class MypageActivity extends AppCompatActivity {
     }
 
 
+
+
+
     private void myStartActivity(Class c) {
         Intent intent = new Intent(this, c);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
+
+
+
+
 
 
 
