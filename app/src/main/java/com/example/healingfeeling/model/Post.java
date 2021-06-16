252fa26b5008e617da84e450728d0b60ac0e4e86
안나영 <dpswpfskdud@naver.com>
2021-06-16 15:31:39 +0900

    server test

--- app/src/main/java/com/example/healingfeeling/model/Post.java
+++ app/src/main/java/com/example/healingfeeling/model/Post.java
@@ -11,13 +11,14 @@ public class Post {
     public String emotion;
     public ArrayList<String> favorite;
     public int register;
+    public Double ratings;
 
 
     public Post(){
         // Default constructor required for calls to DataSnapshot.getValue(FirebasePost.class)
     }
 
-    public Post( String category, String title,String subTitle, String image,String emotion,ArrayList<String> favorite,int register
+    public Post( String category, String title,String subTitle, String image,String emotion,ArrayList<String> favorite,int register,Double ratings
     ) {
         this.category = category;
         this.title = title;
@@ -26,6 +27,7 @@ public class Post {
         this.emotion=emotion;
         this.favorite=favorite;
         this.register=register;
+        this.ratings=ratings;
 
     }
 
